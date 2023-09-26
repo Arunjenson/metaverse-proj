16a464c4780de03a1ba5903e2a7a317aa92ea847
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2023-09-26 16:41:27 +0530

    added insigts section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,12 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from '../styles';
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-3xl bg-[#323f5d]`}>
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    <h1 className="mt-[26px] font-bold leading-[30px] text-white text-2xl">Title {title}</h1>
+    <p className="flex-1 mt-4 font-normal text-lg text-[#b0b0b0] leading-[32px]">{subtitle}</p>
   </div>
 );
 
