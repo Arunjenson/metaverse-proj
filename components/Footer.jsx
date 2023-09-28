7809e7f515f3770b76acde582692d44486a3ffac
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2023-09-28 19:03:24 +0530

    finished the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,45 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+import { socials } from '../constants';
+
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.paddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">Enter the Metaverse</h4>
+        <button type="button" className="flex items-center h-fit py-6 px-6 bg-[#25618b] rounded-[32px] gap-3">
+          <img src="/headset.svg" alt="headset" className="w-6 h-6 object-contain" />
+          <span className="font-normal text-base text-white">ENTER THE METAVERSE</span>
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="flex items-center justify-center flex-wrap gap-4">
+          <h4 className="font-extrabold text-2xl text-white">Metaversus</h4>
+          <p className="font-normal text-sm text-white opacity-50">Copyright &copy; 2023 .All rights reserved</p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                src={social.url}
+                alt={social.name}
+                key={social.name}
+                className="w-6 h-6 object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,35 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { staggerContainer, fadeIn, zoomIn } from '../utils/motion';
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{
+        once: 'false',
+        amount: 0.25,
+      }}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+    >
+      <motion.div variants={fadeIn('right', 'tween', 0.2, 1)} className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col  gradient-05 sm:p-8 p-4 rounded-[32px] border border-[#6a6a6a] relative">
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="text-white font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px]">Arun Jenson</h4>
+          <p className="mt-2 font-normal sm:text-lg text-xs sm:leading-[22px] leading-[16px] text-white">Founder | Metaversus</p>
+        </div>
+        <p className="mt-7 font-normal sm:text-2xl text-lg sm:leading-[45px] leading-[39px] text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore hic vel labore temporibus vero illum atque delectus ipsum natus suscipit!  temporibus vero illum atque delectus ipsum natus suscipit! natus suscipit!.</p>
+      </motion.div>
+      <motion.div variants={fadeIn('left', 'tween', '0.2', '1')} className="relative flex-1 flex justify-center items-center">
+        <img src="/planet-09.png" alt="planet-09" className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]" />
+        <motion.div variants={zoomIn(0.4, 1)} className="lg:block hidden absolute -left-[10%] top-[3%]">
+          <img src="/stamp.png" alt="stamp" className="w-[155px] h-[155px] object-contain" />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
